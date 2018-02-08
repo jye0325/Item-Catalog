c163e2ca3c58c4cb914eac193d46d0b33c9e552c
Jackie Ye <jackieye@Jackies-MBP.fios-router.home>
2018-02-07 22:14:27 -0500

    v0.4.0 Major Bug Fixes, Code Cleanup, and New Files Added

--- database_setup.py
+++ database_setup.py
@@ -1,34 +1,48 @@
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
+from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 
 Base = declarative_base()
 
+
 class User(Base):
 	__tablename__ = 'user'
 	name = Column(String(250), nullable = False)
 	id = Column(Integer, primary_key = True)
 	email = Column(String(250), nullable = False)
-	admin = Column(Boolean, default=False)
-	
+	status = Column(String(100), default="user")
+
+	@property
+	def serialize(self):
+		return {'name':self.name, 'id':self.id, 'email':self.email, 'admin':self.admin}
+
+
 class Category(Base):
 	__tablename__ = 'category'
 	name = Column(String(80), nullable = False)
 	id = Column(Integer, primary_key = True)
-	user = relationship(User)
-	user_id = Column(Integer, ForeignKey('user.id'))
+	#user_id = Column(Integer, ForeignKey('user.id'))
+	#user = relationship(User)
+	
+	@property
+	def serialize(self):
+		return {'name':self.name, 'id':self.id, 'user_id':self.user_id} 
 
 class Item(Base):
 	__tablename__ = 'item'
 	name = Column(String(80), nullable = False)
 	id = Column(Integer, primary_key = True)
 	description = Column(String(500))
-	user = relationship(User)
-	user_id = Column(Integer, ForeignKey('user.id'))
-	category = relationship(Category)
+	#user_id = Column(Integer, ForeignKey('user.id'))
+	#user = relationship(User)
 	category_id = Column(Integer, ForeignKey('category.id'))
+	category = relationship(Category)
+
+	@property
+	def serialize(self):
+		return {'name':self.name, 'id':self.id, 'email':self.email, 'description':self.description, 'category_id':self.category_id, 'user_id':self.user_id}
 	
 
 engine = create_engine('sqlite:///main.db')
--- main.py
+++ main.py
@@ -15,24 +15,20 @@ session = DBSession()
 		Functions:
 		login(),
 		logout(),
-		newCategory(),
-		newItem(),
-		itemMenu(), 
-		editItem(), 
-		deleteItem()
 """
 
 # Routing for login
 @app.route('/login/')
 def login():
     """This returns a webpage for users to login"""
-    return "This is a test page for login()"
+    state = ''.join(random.choice(string.ascii_uppercase+string.digits) for x in xrange(32))
+    login_session['state'] = state
+    return render_template('login.html', STATE=state)
 # Routing for logout
 @app.route('/logout/')
 def logout():
     """This returns a webpage for users to logout"""
     return "This is a test page for logout()"
-
 # Routing for main menu
 @app.route('/')
 @app.route('/main/')
@@ -50,40 +46,76 @@ def categoryItems(category_id):
 	items = session.query(Item).filter_by(category_id=category_id)
 	return render_template('main.html', categories=categories, items=items, category_id=category_id)
 
-@app.route('/main/new/')
+@app.route('/main/newCategory/', methods=['GET','POST'])
 def newCategory():
 	"""Creates a new category -- Requires Admin Privileges"""
-	return "This is a test page for newCategory()"
+	if request.method == 'POST':
+		category = Category(name=request.form['name'])
+		session.add(category)
+		session.commit()
+		flash('Successfully added new category, ' + category.name)
+		return redirect(url_for('mainMenu'))
+	else:
+		return render_template('newCategory.html')
 
-@app.route('/main/newItem/')
-def newUItem():
-	"""Creates a new item for an unspecified category.
-	This WILL have a drop down menu to specify the category."""
-	return "This is a test page for newUItem()"
+@app.route('/main/<int:category_id>/edit', methods=['GET','POST'])
+def editCategory(category_id):
+	"""Edits a specified category."""
+	category = session.query(Category).filter_by(id=category_id).one()
+	if request.method == 'POST':
+		category.name = request.form['name']
+		session.add(category)
+		session.commit()
+		flash('Successfully editted category, ' + category.name)
+		return redirect(url_for('mainMenu'))
+	else:
+		return render_template('editCategory.html', category=category, id=category_id)
 
-@app.route('/main/<int:category_id>/newItem/')
-def newSItem(category_id):
-	"""Creates a new item for a specified category.
-	This WILL NOT have a drop down menu to specify a different category."""
-	return "This is a test page for newSItem()"
+@app.route('/main/<int:category_id>/delete', methods=['GET','POST'])
+def deleteCategory(category_id):
+	"""Deletes a specific category."""
+	category = session.query(Category).filter_by(id=category_id).one()
+	if request.method == 'POST':
+		session.delete(category)
+		session.commit()
+		flash('Successfully deleted category, ' + category.name)
+		return redirect(url_for('mainMenu'))
+	else:
+		return render_template('deleteCategory.html', category=category, id=category_id)
 
 # Routing for item menu
 @app.route('/main/<int:category_id>/item/<int:item_id>/')
 def itemMenu(category_id, item_id):
 	"""Returns details about the particular item in the item page"""
-	return "This is a test page for itemMenu()"
+	category = session.query(Category).filter_by(category_id=category_id).one()
+	item = session.query(Item).filter_by(category_id=category_id, id=item_id).one()
+	return render_template('item.html', category=category, item=item)
+
+@app.route('/main/<int:category_id>/newItem/', methods=['GET','POST'])
+def newItem(category_id):
+	"""Creates a new item for an unspecified category."""
+	if request.method == 'POST':
+		item = Item(name=request.form['name'], category_id=category_id, description=request.form['description'])
+		session.add(item)
+		session.commit()
+		flash('Successfully added new category, ' + item.name)
+		return redirect(url_for('mainMenu'))
+	else:
+		return render_template('newItem.html', id=category_id)
 
 @app.route('/main/<int:category_id>/item/<int:item_id>/edit/', methods=['GET','POST'])
 def editItem(category_id, item_id):
 	"""Edit the details about the particular item"""
 	item = session.query(Item).filter_by(category_id=category_id, id=item_id).one()
 	if request.method == 'POST':
+		item.name=request.form['name']
+		item.description=request.form['description']
 		session.add(item)
 		session.commit()
-		flash('Item was successfully editted!')
+		flash('Successfully editted item, ' + item.name)
 		return redirect(url_for('mainMenu'))
 	else:
-		return render_template('edit.html', item=item)
+		return render_template('editItem.html', item=item, id=category_id, item_id=item_id)
 	
 @app.route('/main/<int:category_id>/item/<int:item_id>/delete', methods=['GET', 'POST'])
 def deleteItem(category_id, item_id):
@@ -92,10 +124,42 @@ def deleteItem(category_id, item_id):
 	if request.method == 'POST':
 		session.delete(item)
 		session.commit()
-		flash('Item was successfully deleted!')
+		flash('Successfully deleted item, ' + item.name)
 		return redirect(url_for('mainMenu'))
 	else:
-		return render_template('delete.html', item=item)
+		return render_template('deleteItem.html', item=item, id=category_id, item_id=item_id)
+"""
+@app.route('/main/<int:category_id>/newItem/', methods=['GET','POST'])
+def newSItem(category_id):
+	#Creates a new item for a specified category.
+	#This WILL NOT have a drop down menu to specify a different category.
+	if request.method == 'POST':
+		item = Item(name=request.form['name'], description=request.form['description'], category_id=category_id)
+		session.add(item)
+		session.commit()
+		flash('Successfully added new item, ' + item.name)
+		return render_template('main.html')
+	else:
+		return render_template('new.html')
+"""
+
+#JSON 
+@app.route('/main/JSON')
+def mainMenuJSON():
+	category = session.query(Category).all()
+	return jsonify(Category=[i.serialize for i in category])
+
+@app.route('/main/<int:category_id>/item/JSON')
+def itemMenuJSON(category_id):
+	category = session.query(Category).all()
+	item = session.query(Item).filter_by(category_id=category_id).all()
+	return jsonify(Item=[i.serialize for i in item])
+
+@app.route('/main/<int:category_id>/item/<int:item_id>/JSON')
+def specificItemJSON(category_id, item_id):
+	category = session.query(Category).all()
+	item = session.query(Item).filter_by(id=item_id, category_id=category_id).one()
+	return jsonify(Item=item.serialize)
 
 if __name__ == '__main__':
 	app.secret_key = 'root'
